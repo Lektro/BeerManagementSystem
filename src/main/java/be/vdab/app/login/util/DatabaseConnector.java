11d982cc00dc9bb13f1bf2d5b762f62a641358f5
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-07-15 13:19:01 +0200

    Removed redundant code

--- src/main/java/be/vdab/app/login/util/DatabaseConnector.java
+++ src/main/java/be/vdab/app/login/util/DatabaseConnector.java
@@ -9,19 +9,16 @@ import static be.vdab.app.login.util.LoginCredentials.*;
 public class DatabaseConnector {
     public static Object dbConnect() {
 
-        Connection conn = null;
+        Connection conn;
 
         try {
             conn = DriverManager.getConnection(url, user, pass);
             System.out.println("Connection established!");
 
-            if (conn == null) {
 
-            }
         } catch (Exception e) {
             System.out.println(e);
             System.out.println("Connection could not be established!");
-            return null;
         }
         return null;
     }
