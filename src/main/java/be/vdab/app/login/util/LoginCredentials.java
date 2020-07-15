6d9cb1631aa2343ff1f04c9a4a614a90ed8d141e
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-07-16 00:10:45 +0200

    hmm not working removing login details

--- src/main/java/be/vdab/app/login/util/LoginCredentials.java
+++
@@ -1,8 +0,0 @@
-package be.vdab.app.login.util;
-
-public class LoginCredentials {
-
-    public static final String url = "jdbc:mysql://localhost:3306/beersdb";
-    public static final String user = "root";
-    public static final String pass = "Student4J";
-}
