1cf0328957c8987130a5e3882944f9be3082393d
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-07-17 10:53:42 +0200

    Initial setup for CLI menu structure

--- src/main/java/be/vdab/app/main/AppMain.java
+++ src/main/java/be/vdab/app/main/AppMain.java
@@ -1,10 +1,12 @@
 package be.vdab.app.main;
 
-import static be.vdab.app.searches.DatabaseSearchBrewersOnTurnover.dbConnectSearchOnBrewerTurnover;
-
 public class AppMain {
 
     public static void main(String[] args) {
+
+        mainMenu();
+    }
+}
 
         // System.out.println("Test");
         // dbConnectSearchOnName();
@@ -13,17 +15,19 @@ public class AppMain {
         // dbConnectSearchOnName();
         // dbConnectSearchOnID();
         // dbConnectChangePrice();
-        dbConnectSearchOnBrewerTurnover();
+        // dbConnectSearchOnBrewerTurnover();
         // new DatabaseSearchOnName();
         // give all beers by producers with selected turnover? sql merge?
         // adjust price?? gives --> java.util.InputMismatchException
         // adjust price works could use a unit test I suppose
         //
-    }
-}
+
+
 
 // testrun on Primeur kriek  ID Number: 1076 5.0 % VOL  2.55 EUR  50 Pcs in Stock  Brewer ID = 11 Category ID = 22
 // after update --> Primeur kriek  ID Number: 1076 5.0 % VOL  2.55 EUR  555 Pcs in Stock  Brewer ID = 11 Category ID = 22
 // succes with update price
 
 // next option SELECT `brewers`.`Name`, `brewers`.`Turnover` FROM `beersdb`.`brewers` WHERE Turnover > '50000'; filter on turnover and get all their beers
+//succes
+// started building on the CLI menu structure
---
+++ src/main/java/be/vdab/app/main/MainMenu.java
@@ -0,0 +1,44 @@
+package be.vdab.app.main;
+
+public class MainMenu {
+
+    private void mainMenu {
+        // Local variable
+        int swValue;
+
+        // Display menu graphics
+        System.out.println("=================================");
+        System.out.println("|     Beer Inventory System     |");
+        System.out.println("=================================");
+        System.out.println("| Options:                      |");
+        System.out.println("| 1. Search on Name             |");
+        System.out.println("| 2. Search on ID               |");
+        System.out.println("| 2. Search on Alcohol VOL      |");
+        System.out.println("| 3. Search Brewers on Turnover |");
+        System.out.println("| 4. Change Beer Price by ID    |");
+        System.out.println("| 5. Change Beer Stock by ID    |");
+        System.out.println("| 6. ....                       |");
+        System.out.println("| 7. ....                       |");
+        System.out.println("| 8. ....                       |");
+        System.out.println("| 9. ....                       |");
+        System.out.println("| 0. Exit                       |");
+        System.out.println("=================================");
+        swValue = Keyin.inInt(" Select option: ");
+
+        // Switch construct
+        switch (swValue) {
+            case 1:
+
+                break;
+            case 2:
+                System.out.println("Option 2 selected");
+                break;
+            case 3:
+                System.out.println("Exit selected");
+                break;
+            default:
+                System.out.println("Invalid selection");
+                break; // This break is not really necessary
+        }
+    }
+}
